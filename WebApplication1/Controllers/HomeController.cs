8cfdae9778d584307add37822b9c285667a4e7aa
Debraj <Debraj@LAPTOP-HK5B9AQF>
2023-11-25 23:24:10 +0530

    Add project files.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            // ViewBag.Message = "Your application description page. This is for testing.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            // ViewBag.Message = "Your contact page. This is test purpose";
+
+            return View();
+        }
+
+        public ActionResult Career()
+        {
+            // ViewBag.Message = "This is your career page";
+            return View();
+        }
+
+        public ActionResult Product()
+        {
+            //ViewBag.Message = "This is your Product page";
+            return View();
+        }
+    }
+}
